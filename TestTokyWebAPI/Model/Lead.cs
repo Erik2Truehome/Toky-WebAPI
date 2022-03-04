210ef8f315f645b8eba06a76497ef923348488ca
Erik2Truehome <100309791+Erik2Truehome@users.noreply.github.com>
2022-03-04 09:53:17 -0600

    Modificacion para que todos los atributos en  la clase inicien con Mayuscula

--- TestTokyWebAPI/Model/Lead.cs
+++ TestTokyWebAPI/Model/Lead.cs
@@ -7,6 +7,6 @@
         public string Email { get; set; }
         public string Name { get; set; }
         public string Lastname { get; set; }
-        public Telephone telephone { get; set; }
+        public Telephone Telephone { get; set; }
     }
 }
